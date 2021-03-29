db3d7f366a45aec13aceb1beaa350be49ed600ce
KamilaOlszewska <76682285+KamilaOlszewska@users.noreply.github.com>
2021-03-29 22:31:45 +0200

    wykonane zadanie

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
 /* Here goes your JS code */
+const showPopupButton = document.getElementById("show-popup-button")
+const showPopupButtonMain = document.getElementById("show-popup-button-main")
+showPopupButton.addEventListener("click", ev => {
+    showPopupButtonMain.classList.add("hidden");
+    showPopupFormMain.classList.remove("hidden");
+});
+
+
+const showPopupForm = document.getElementById("show-popup-form")
+const showPopupFormMain = document.getElementById("show-popup-form-main")
+const showPopupSuccessMain = document.getElementById("show-popup-success-main")
+const faCheck = document.querySelector(".fa-check")
+const formInputLabel = document.querySelector(".form-input-label")
+
+showPopupForm.addEventListener("submit", ev => {
+    ev.preventDefault()
+    showPopupFormMain.classList.add("hidden");
+    showPopupSuccessMain.classList.remove("hidden");
+
+});
+formInputLabel.addEventListener("click", ev=>{
+    faCheck.style.color = "black"
+})
